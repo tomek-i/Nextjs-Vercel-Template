2a857f403e4db56f37fe0fb2ead366cc7810b45a
tomek-i <tomek.iwainski@gmail.com>
2024-10-22 14:58:48 +1100

    million lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,40 @@
-import withBundleAnalyzer from "@next/bundle-analyzer"
-import withPlugins from "next-compose-plugins"
-import path from "path"
-import { env } from "./env/env.mjs"
+import MillionLint from '@million/lint';
+import withBundleAnalyzer from "@next/bundle-analyzer";
+import withPlugins from "next-compose-plugins";
+import path from "path";
+import { env } from "./env/env.mjs";
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+const config = withPlugins([[withBundleAnalyzer({
+  enabled: env.ANALYZE
+})]], {
   reactStrictMode: true,
-  experimental: { instrumentationHook: true },
+  experimental: {
+    instrumentationHook: true
+  },
   transpilePackages: ["@"],
   webpack: (config, _) => {
-    config.resolve.alias["@"] = path.resolve(process.cwd(), "src")
-    return config
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
+    return config;
   },
   rewrites() {
-    return [
-      { source: "/healthz", destination: "/api/health" },
-      { source: "/api/healthz", destination: "/api/health" },
-      { source: "/health", destination: "/api/health" },
-      { source: "/ping", destination: "/api/health" },
-    ]
-  },
-})
-
-export default config
+    return [{
+      source: "/healthz",
+      destination: "/api/health"
+    }, {
+      source: "/api/healthz",
+      destination: "/api/health"
+    }, {
+      source: "/health",
+      destination: "/api/health"
+    }, {
+      source: "/ping",
+      destination: "/api/health"
+    }];
+  }
+});
+export default MillionLint.next({
+  rsc: true
+})(config);
